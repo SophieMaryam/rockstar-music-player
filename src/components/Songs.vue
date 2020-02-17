c173ebda5b6d86117b94eb42b8b9a38f3c7da2b3
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-02-17 13:52:18 +0100

    Update data to be set in localstorage

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="flex mt-5">
     <div>
-      <h3 class="playlist-name font-weight-bold"> {{ getPlaylistName }}</h3>
-      <h5 class="text-white font-weight-bold">Select a song to add it to your playlist!</h5>
-      <div class="mt-4 mb-3 text-white">
-        <label
-          for="new-song"
-          class="text-white mr-2">
-            Add a Song:
+      <h3 class="playlist-name font-weight-bold">{{ getPlaylistName }}</h3>
+      <h5 class="text-white font-weight-bold">
+        Select a song to add it to your playlist!
+      </h5>
+      <div class="mt-4 mb-3 text-black">
+        <label for="new-song" class="text-black mr-2">
+          Add a Song:
         </label>
         <input
           v-model="input"
           class="input-field"
           placeholder="i.e. Mr. Brightside"
           required
-        >
+        />
       </div>
-      <div 
-        v-if="input != ''"
-        class="mt-2 text-white"
-      >
+      <div v-if="input" class="mt-2 text-white">
         <div>
           <ul>
             <li
-              class=""
-              @click="addSelectedSongsToPlaylist(song)"
               v-for="song in filterAllSongs"
               :key="song.id"
+              @click="addSongToPlaylist(song)"
             >
               {{ song.name }}
             </li>
@@ -36,41 +32,42 @@
     </div>
     <div class="margins">
       <h3 class="font-weight-bold">Your Songs:</h3>
-      <ul>
-        <li
-          class="text-white mt-2"
-          v-for="(addedSong, index) in getSelectedPlaylistSongs"
-          :key="addedSong.id"
-        >
-          {{ addedSong }}
-          <button 
-            class="btn remove-button ml-3 text-uppercase rounded-0 font-weight-bold right" @click="getSelectedPlaylistSongs.splice(index, 1)">
+      <div class="d-flex">
+        <ul>
+          <li
+            class="mt-5"
+            v-for="(addedSong, index) in selectedPlaylistSongs"
+            :key="addedSong.id"
+          >
+            {{ addedSong.name }}
+            <button
+              class="btn remove-button ml-3 text-uppercase rounded-0 font-weight-bold right"
+              @click="selectedPlaylistSongs.splice(index, 1)"
+            >
               Remove
-          </button>
-        </li>
-      </ul>
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import RockstarJson from "../common/rockstars.json";
+import MusicJSON from "../common/musicList.json";
 
 export default {
   name: "Songs",
   data() {
     return {
-      data: RockstarJson,
-      allSongs: [],
+      allSongs: MusicJSON.songs,
       input: this.$store.getters.input,
       playlistName: "",
-      allPlaylists: this.$store.getters.allPlaylists,
-      selectedPlayListSongsNames: []
+      selectedPlaylistSongs: []
     };
   },
   mounted() {
-    this.getAllSongs();
-    this.input = this.$store.getters.input
+    this.input = this.$store.getters.input;
   },
   computed: {
     filterAllSongs() {
@@ -80,67 +77,75 @@ export default {
     },
     setInput: {
       get() {
-        return this.$store.state.input
+        return this.$store.state.input;
       },
       set() {
-        this.$store.commit("setInputValue")
+        this.$store.commit("setInputValue");
       }
     },
     getPlaylistName() {
       this.playlistName = this.$store.getters.playlistName;
       return this.playlistName;
-    },
-    getSelectedPlaylistSongs() {
-      const selectedPlaylist = this.allPlaylists.find(playList => this.playlistName === playList.name);
-      this.selectedPlayListSongsNames = selectedPlaylist.songs.map(song => song.name);
-      return this.selectedPlayListSongsNames;
     }
   },
   methods: {
-    getAllSongs() {
-      this.allSongs = this.data.songs;
-    },
-    addSelectedSongsToPlaylist(song) {
-      return this.allPlaylists.filter(playList => {
-        if (playList.name === this.playlistName) {
-          playList.songs.push(song); 
-        }
-        this.resetInputField();
+    addSongToPlaylist(selectedSong) {
+      let playlists = JSON.parse(localStorage.getItem("playlists")) || [];
+      let filteredPlaylist = playlists.find(playlist => {
+        return playlist.name === this.$store.getters.playlistName;
       });
+      if (filteredPlaylist) {
+        filteredPlaylist.songs.push({ name: selectedSong });
+        localStorage.setItem("playlists", JSON.stringify(playlists));
+      }
+      this.resetInputField();
+      this.getSelectedPlaylistSongs(filteredPlaylist);
+    },
+    getSelectedPlaylistSongs(filteredPlaylist) {
+      if (filteredPlaylist.name === this.$store.getters.playlistName) {
+        this.selectedPlaylistSongs = filteredPlaylist.songs.map(
+          song => song.name
+        );
+        return this.selectedPlaylistSongs;
+      }
     },
     removeSong(index) {
-      this.selectedPlayListSongsNames.splice(index, 1)
-      console.log(this.selectedPlayListSongsNames)
+      this.selectedPlaylistSongs.splice(index, 1);
+      localStorage.setItem("playlists", JSON.stringify(playlists));
+    },
+    filteredPlaylist() {
+      let playlists = JSON.parse(localStorage.getItem("playlist")) || [];
+      let filteredPlaylist = playlists.find(playlist => {
+        return playlist.name === this.playlistName;
+      });
     },
     resetInputField() {
       this.input = "";
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .remove-button {
-    color: black;
-    background-color: #FFE000;
-    width: 100px;
-    font-size: 10px;
-  }
+/* .remove-button {
+  color: white;
+  background-color: black;
+  width: 100px;
+  font-size: 10px;
+} */
 
+.margins {
+  margin-left: 50px;
+}
+
+@media only screen and (max-width: 1199px) {
   .margins {
-    margin-left: 50px;
+    margin-left: 0px;
+    margin-top: 100px;
   }
-  
-  @media only screen and (max-width: 1199px) {
-    .margins {
-      margin-left: 0px;
-      margin-top: 100px;
-    }
-  }
+}
 
-  .right {
-    float: right;
-  }
+.right {
+  float: right;
+}
 </style>
-
-
