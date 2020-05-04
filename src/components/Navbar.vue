5384be373ceb1d4478e2690e53091bb50bf6445d
Sophie van Wersch <sophievanwersch@hotmail.com>
2020-05-04 10:13:10 +0200

    Fix broken mobile menu dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,32 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-dark background-color">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div>
-      <router-link class="navbar-brand text-uppercase font-weight-bold" to="/">Music App</router-link >
+      <router-link class="navbar-brand text-uppercase font-weight-bold" to="/"
+        >Music App</router-link
+      >
     </div>
-    <button class="navbar-toggler custom-toggler"
-      type="button" 
+    <button
+      class="navbar-toggler"
+      type="button"
+      @click.stop="toggleNavbar"
       data-toggle="collapse"
-      data-target="#navbarToggle" aria-controls="navbarToggle"
-      aria-expanded="false" 
+      data-target="#navbarToggle"
+      aria-controls="navbarToggle"
+      aria-expanded="false"
       aria-label="Toggle navigation"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarToggle">
+    <div
+      class="collapse navbar-collapse"
+      id="navbarToggle"
+      :class="{ show: show }"
+    >
       <ul class="navbar-nav ml-auto smooth-scroll">
         <li class="ml-3 nav-link">
-            <router-link class="text-color-black text-uppercase" to="/playlists">Playlists</router-link>
+          <router-link class="text-color-black text-uppercase" to="/playlists"
+            >Playlists</router-link
+          >
         </li>
       </ul>
     </div>
@@ -23,13 +34,19 @@
 </template>
 
 <script>
-  export default {
-    name: "Navbar"
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    toggleNavbar() {
+      this.show = !this.show;
+    }
   }
+}
 </script>
 
-<style scoped>
-
-</style>
-
-
+<style scoped></style>
